2c312f86bbb846ea3ac2df7bd35b6795572020f5
janvanrijn <janvanrijn@gmail.com>
2021-03-18 13:03:27 +0100

    updated classifier script

--- codequality/examples/make_classifier.py
+++ codequality/examples/make_classifier.py
@@ -19,31 +19,23 @@ def parse_args():
     return parser.parse_args()
 
 
-def get_data_and_labels(frame: pd.DataFrame, smell_type: str, random_seed: int):
-    frame['smell'] = frame['smell'].apply(lambda value: True if value == smell_type else False)
-
-    df_majority = frame[~frame['smell']]
-    df_minority = frame[frame['smell']]
-    df_majority_downsampled = sklearn.utils.resample(
-        df_majority, replace=True, n_samples=len(df_minority),
-        random_state=random_seed)
-    frame = pd.concat([df_majority_downsampled, df_minority])
-
-    y = frame['smell'].to_numpy(dtype=bool)
-
+def get_data_and_labels(frame: pd.DataFrame, severity_labels: typing.List[str]):
+    y = frame['severity'].apply(lambda value: True if value in severity_labels else False).to_numpy(dtype=bool)
     logging.info("Dtypes:\n" + str(frame.dtypes))
     logging.info("Values Count:\n" + str(frame['smell'].value_counts()))
     frame = frame.drop([
         'severity',
-        'CommitHashPrefix',
+        'CommitHash',
         'Name',
         'smell'
     ], axis=1)
+    unique, counts = np.unique(y, return_counts=True)
+    logging.info("Class distribution: %s" % str(dict(zip(unique, counts))))
     return frame.to_numpy(dtype=float), y
 
 
-def evaluate(frame: pd.DataFrame, smell_type: str, scorers: typing.List[str], random_seed: int):
-    data, labels = get_data_and_labels(frame, smell_type, random_seed)
+def evaluate(frame: pd.DataFrame, scorers: typing.List[str], severity_labels: typing.List[str], random_seed: int):
+    data, labels = get_data_and_labels(frame, severity_labels)
     classifier = sklearn.pipeline.make_pipeline(
         sklearn.impute.SimpleImputer(strategy='constant', fill_value=-1.0),
         sklearn.feature_selection.VarianceThreshold(),
@@ -61,7 +53,6 @@ def run(args):
     scorers = ['accuracy', 'precision', 'recall']
 
     for idx, file in enumerate(files):
-        file_name = os.path.splitext(file)[0]
         file_extension = os.path.splitext(file)[-1]
         if os.path.splitext(file)[-1] != '.csv':
             logging.info("skipping file: %s (extension %s)" % (file, file_extension))
@@ -69,7 +60,7 @@ def run(args):
 
         file = os.path.join(args.input_dir, file)
         frame = pd.read_csv(file)
-        all_results = evaluate(frame, file_name, scorers, random_seed)
+        all_results = evaluate(frame, scorers, ['minor', 'major', 'critical'], random_seed)
         for scorer in scorers:
             result = all_results["test_%s" % scorer]
             logging.info("%s: %f +/- %f" % (
